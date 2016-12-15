a2dce89cff2b53b2706e89d663eb59ff75e1d413
Pierre Rothmaler <me@pierre.me>
2016-12-15 10:30:41 +0100

    using graphics instead of custom drawn buttons

--- PasscodeLock/Views/PasscodeSignButton.swift
+++ PasscodeLock/Views/PasscodeSignButton.swift
@@ -8,96 +8,96 @@
 
 import UIKit
 
-@IBDesignable
+//@IBDesignable
 public class PasscodeSignButton: UIButton {
     
-    @IBInspectable
+//    @IBInspectable
     public var passcodeSign: String = "1"
     
-    @IBInspectable
-    public var borderColor: UIColor = UIColor.whiteColor() {
-        didSet {
-            setupView()
-        }
-    }
-    
-    @IBInspectable
-    public var borderRadius: CGFloat = 30 {
-        didSet {
-            setupView()
-        }
-    }
-    
-    @IBInspectable
-    public var highlightBackgroundColor: UIColor = UIColor.clearColor() {
-        didSet {
-            setupView()
-        }
-    }
-    
-    public override init(frame: CGRect) {
-        
-        super.init(frame: frame)
-        
-        setupView()
-        setupActions()
-    }
-    
-    public required init?(coder aDecoder: NSCoder) {
-        
-        super.init(coder: aDecoder)
-        
-        setupActions()
-    }
-    
-    public override func intrinsicContentSize() -> CGSize {
-        
-        return CGSizeMake(60, 60)
-    }
-    
-    private var defaultBackgroundColor = UIColor.clearColor()
-    
-    private func setupView() {
-        
-        layer.borderWidth = 1
-        layer.cornerRadius = layer.frame.height/2
-        layer.borderColor = borderColor.CGColor
-        
-        if let backgroundColor = backgroundColor {
-            
-            defaultBackgroundColor = backgroundColor
-        }
-    }
-    
-    private func setupActions() {
-        
-        addTarget(self, action: #selector(self.handleTouchDown), forControlEvents: .TouchDown)
-        addTarget(self, action: #selector(self.handleTouchUp), forControlEvents: [.TouchUpInside, .TouchDragOutside, .TouchCancel])
-    }
-    
-    func handleTouchDown() {
-        
-        animateBackgroundColor(highlightBackgroundColor)
-    }
-    
-    func handleTouchUp() {
-        
-        animateBackgroundColor(defaultBackgroundColor)
-    }
-    
-    private func animateBackgroundColor(color: UIColor) {
-        
-        UIView.animateWithDuration(
-            0.3,
-            delay: 0.0,
-            usingSpringWithDamping: 1,
-            initialSpringVelocity: 0.0,
-            options: [.AllowUserInteraction, .BeginFromCurrentState],
-            animations: {
-                
-                self.backgroundColor = color
-            },
-            completion: nil
-        )
-    }
+//    @IBInspectable
+//    public var borderColor: UIColor = UIColor.whiteColor() {
+//        didSet {
+//            setupView()
+//        }
+//    }
+//    
+//    @IBInspectable
+//    public var borderRadius: CGFloat = 30 {
+//        didSet {
+//            setupView()
+//        }
+//    }
+//    
+//    @IBInspectable
+//    public var highlightBackgroundColor: UIColor = UIColor.clearColor() {
+//        didSet {
+//            setupView()
+//        }
+//    }
+//
+//    public override init(frame: CGRect) {
+//        
+//        super.init(frame: frame)
+//        
+//        setupView()
+//        setupActions()
+//    }
+//    
+//    public required init?(coder aDecoder: NSCoder) {
+//        
+//        super.init(coder: aDecoder)
+//        
+//        setupActions()
+//    }
+//    
+//    public override func intrinsicContentSize() -> CGSize {
+//        
+//        return CGSizeMake(60, 60)
+//    }
+//    
+//    private var defaultBackgroundColor = UIColor.clearColor()
+//
+//    private func setupView() {
+//        
+//        layer.borderWidth = 1
+//        layer.cornerRadius = layer.frame.height/2
+//        layer.borderColor = borderColor.CGColor
+//
+//        if let backgroundColor = backgroundColor {
+//            
+//            defaultBackgroundColor = backgroundColor
+//        }
+//    }
+//
+//    private func setupActions() {
+//        
+//        addTarget(self, action: #selector(self.handleTouchDown), forControlEvents: .TouchDown)
+//        addTarget(self, action: #selector(self.handleTouchUp), forControlEvents: [.TouchUpInside, .TouchDragOutside, .TouchCancel])
+//    }
+//    
+//    func handleTouchDown() {
+//
+//        animateBackgroundColor(highlightBackgroundColor)
+//    }
+//
+//    func handleTouchUp() {
+//
+//        animateBackgroundColor(defaultBackgroundColor)
+//    }
+//
+//    private func animateBackgroundColor(color: UIColor) {
+//        
+//        UIView.animateWithDuration(
+//            0.3,
+//            delay: 0.0,
+//            usingSpringWithDamping: 1,
+//            initialSpringVelocity: 0.0,
+//            options: [.AllowUserInteraction, .BeginFromCurrentState],
+//            animations: {
+//                
+//                self.backgroundColor = color
+//            },
+//            completion: nil
+//        )
+//    }
 }
